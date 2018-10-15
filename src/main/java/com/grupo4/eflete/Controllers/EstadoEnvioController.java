dbe66f63121cea3abca078a55af5e5369716147a
Damian Lattenero <damianlattenero@gmail.com>
2018-10-15 17:26:05 -0300

    Se modifica la respuesta de envio para que pueda verse correctamente

--- src/main/java/com/grupo4/eflete/Controllers/EstadoEnvioController.java
+++ src/main/java/com/grupo4/eflete/Controllers/EstadoEnvioController.java
@@ -4,6 +4,8 @@ import com.grupo4.eflete.Model.Envio;
 import com.grupo4.eflete.Model.EstadoEnvio;
 import com.grupo4.eflete.Repositories.EnvioRepository;
 import com.grupo4.eflete.Repositories.EstadoEnvioRepository;
+import com.grupo4.eflete.dtos.EstadoEnvioDto;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,23 +22,25 @@ public class EstadoEnvioController {
     @Autowired
     private EnvioRepository envioRepository;
 
+    private ModelMapper modelMapper = new ModelMapper();
+
     @GetMapping("/estadoenvios/{id}")
     public EstadoEnvio getEstadoEnvioById(@PathVariable Long id) {
         return estadoEnvioRepository.getOne(id);
     }
 
     @PostMapping("/estadoenvios/{idEnvio}")
-    @ResponseBody public ResponseEntity<Envio> createEstadoEnvio(@RequestBody EstadoEnvio estadoEnvio, @PathVariable Long idEnvio) {
+    @ResponseBody public ResponseEntity<EstadoEnvioDto> createEstadoEnvio(@RequestBody EstadoEnvio estadoEnvio, @PathVariable Long idEnvio) {
         Envio getEnvio = envioRepository.getOne(idEnvio);
         estadoEnvio.setEnvio(getEnvio);
         EstadoEnvio savedEstado = estadoEnvioRepository.save(estadoEnvio);
 
         envioRepository.updateEstadoActual(savedEstado.getId(), getEnvio.getId());
 
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-                .buildAndExpand(savedEstado.getId()).toUri();
+        EstadoEnvioDto estadoEnvioDto = modelMapper.map(savedEstado, EstadoEnvioDto.class);
 
-        return ResponseEntity.created(location).build();
+
+        return ResponseEntity.ok(estadoEnvioDto);
 
     }
 
